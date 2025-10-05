9efa608b0e06c2a49f73c65a67df7749eefd527c
Yash Agarwal <yashagarwal7088@gmail.com>
2025-10-05 15:27:33 +0530

    updated login screen, added berkshire font

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -8,40 +8,44 @@ import {
 } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {colors} from '../../src/styles'
+import { colors } from '../../src/styles';
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   return (
-    <SafeAreaView style={styles.container}>
-      <Pressable>
-        <Text style={styles.pressableNav}>Skip</Text>
+    <SafeAreaView>
+      <Pressable style={[{ justifyContent: 'center', alignItems: 'flex-end' }]}>
+        <Text style={[styles.pressableNav, { fontSize: 20 }]}>Skip</Text>
       </Pressable>
-      <Text style={styles.header}>Welcome to Oxiauraa</Text>
-      <TextInput
-        placeholder="Email / Phone number"
-        value={email}
-        onChangeText={setEmail}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        style={styles.input}
-      />
-      <View style={styles.child}>
-        <Pressable>
-          <Text style={styles.pressableNav}>Forgot Password?</Text>
-        </Pressable>
-      </View>
+      <View style={styles.container}>
+        <Text style={styles.header}>Welcome to Oxiauraa</Text>
+        <TextInput
+          placeholder="Email / Phone number"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+          keyboardType="email-address"
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          style={styles.input}
+          secureTextEntry
+        />
+        <View style={styles.child}>
+          <Pressable>
+            <Text style={styles.pressableNav}>Forgot Password?</Text>
+          </Pressable>
+        </View>
 
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Register</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonFilled}>
+          <Text style={styles.buttonTextFilled}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonOutlined}>
+          <Text style={styles.buttonTextOutlined}>Register</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
@@ -51,39 +55,56 @@ export default LoginScreen;
 const styles = StyleSheet.create({
   container: {
     // flex: 1,
-    height:"100%",
+    height: '90%',
     justifyContent: 'center',
     alignItems: 'center',
     gap: 10,
   },
   header: {
-    fontSize:25,
-    fontWeight:"bold",
-    fontFamily:"Berkshire Swash",
+    fontSize: 25,
+    fontWeight: 'bold',
+    fontFamily: 'BerkshireSwash-Regular',
+    marginBottom: 40,
   },
   input: {
-    borderWidth:1,
-    borderColor:colors.border,
-    padding:20,
-    borderRadius:10,
-    width:"80%",
-
+    borderWidth: 1,
+    borderColor: colors.border,
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
   },
   pressableNav: {
-    color:colors.fontLight,
-    textDecorationLine:'underline',
+    color: colors.fontLight,
+    textDecorationLine: 'underline',
+    margin: 10,
   },
-  button:{
-    backgroundColor:colors.primary,
-    padding:18,
-    borderRadius:10,
-    // flexGrow:1, 
-
+  buttonFilled: {
+    backgroundColor: colors.primary,
+    padding: 18,
+    borderRadius: 10,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // margin:5,
+    // flexGrow:1,
   },
-  buttonText:{
-    fontWeight:"bold",
-    fontSize:14,
-    color:"white",
-    
-  }
+  buttonOutlined: {
+    borderColor: colors.primary,
+    borderWidth: 1,
+    padding: 18,
+    borderRadius: 10,
+    width: '80%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonTextFilled: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: 'white',
+  },
+  buttonTextOutlined: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: colors.primary,
+  },
 });
