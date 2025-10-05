82753fc6e788d51996e33f6bb8762237f1c0fb06
Yash Agarwal <yashagarwal7088@gmail.com>
2025-10-05 13:53:33 +0530

    update app.jsx,login and installed safe-area-context

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import LoginScreen from './src/screens/LoginScreen'
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <LoginScreen />
+      {/* <Text>App</Text> */}
     </View>
   )
 }
---
+++ src/styles.js
@@ -0,0 +1,6 @@
+export const colors = {
+    primary:'#E76832',
+    border:'#E1E2E7',
+    fontLight:'#8B8B8B',
+    
+}
